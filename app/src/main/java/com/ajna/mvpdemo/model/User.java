ef1050fd6e6301a618216fc1c917914d87388b64
zajan <aniaaa94@gmail.com>
2019-03-21 19:55:22 +0100

    add User.java class (model)

---
+++ app/src/main/java/com/ajna/mvpdemo/model/User.java
@@ -0,0 +1,24 @@
+package com.ajna.mvpdemo.model;
+
+public class User {
+    private String login, password;
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
